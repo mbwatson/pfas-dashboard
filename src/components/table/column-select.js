f0324aba5efdf8eeed5da6ae864dffe31424b8cb
Matt Watson <mwatson@renci.org>
2024-05-16 23:48:19 -0400

    improve column visibility toggling by group

--- src/components/table/column-select.js
+++ src/components/table/column-select.js
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types'
 import {
   Checkbox,
   Dropdown,
+  List,
   Menu,
   ListItem,
+  ListItemDecorator,
+  ListSubheader,
   MenuButton,
   MenuList,
-  Stack,
-  Typography,
 } from '@mui/joy'
 import {
   KeyboardArrowDown as ChevronDownIcon,
@@ -15,6 +16,25 @@ import {
 } from '@mui/icons-material'
 
 export const ColumnSelect = ({ table }) => {
+  const handleClickToggleColumnGroup = ({ columns }) => event => {
+    // we have to make some assumptions here.
+    // we perform different actions based on whether
+    // checkboxes are currently selected.
+    // we have logic below to check the checkbox if
+    // some column in the group is visible; no need to
+    // duplicate that here. so we'll just look at the
+    // checked state of the clicked checkbox.
+    if (event.target.checked) {
+      columns
+        .filter(col => !col.getIsVisible())
+        .forEach(col => col.toggleVisibility())
+      return
+    }
+    columns
+      .filter(col => col.getIsVisible())
+      .forEach(col => col.toggleVisibility())
+  }
+
   return (
     <Dropdown>
       <MenuButton
@@ -24,33 +44,51 @@ export const ColumnSelect = ({ table }) => {
       >Columns</MenuButton>
       <Menu aria-labelledby="column-select" size="sm">
         <MenuList sx={{
-          maxWidth: '200px',
-          maxHeight: '66vh',
+          width: '250px',
+          maxWidth: '250px',
+          height: '50vh',
+          maxHeight: '600px',
           overflowY: 'auto',
         }}>
           {
             table.getAllColumns().map(columnGroup => {
+              const selectedColumnCount = columnGroup.columns
+                .filter(col => col.getIsVisible())
+                .length
+              const checked = selectedColumnCount > 0
+              const checkboxColor = selectedColumnCount === columnGroup.columns.length
+                ? 'primary' : 'neutral'
               return (
-                <Stack key={ columnGroup.id }>
-                  <Typography
-                    level="body-xs"
-                    textTransform="uppercase"
-                    sx={{ p: 1 }}
-                  >{ columnGroup.id.toUpperCase() }</Typography>
-                  {
-                    columnGroup.columns.map(column => (
-                      <ListItem key={ column.id }>
-                        <Checkbox
-                          label={ column.id }
-                          checked={ column.getIsVisible() }
-                          onChange={ column.getToggleVisibilityHandler() }
-                          variant="soft"
-                          size="sm"
-                        />
-                      </ListItem>
-                    ))                    
-                  }
-                </Stack>
+                <ListItem nested key={ columnGroup.id }>
+                  <ListSubheader sticky>
+                    <ListItemDecorator>
+                      <Checkbox
+                        checked={ checked }
+                        onChange={ handleClickToggleColumnGroup(columnGroup) }
+                        variant="soft"
+                        color={ checkboxColor }
+                        size="sm"
+                      />
+                    </ListItemDecorator>
+                    { columnGroup.id.toUpperCase() }
+                  </ListSubheader>
+                  <List>
+                    {
+                      columnGroup.columns.map(column => (
+                        <ListItem key={ column.id }>
+                          <ListItemDecorator />
+                          <Checkbox
+                            label={ column.id }
+                            checked={ column.getIsVisible() }
+                            onChange={ column.getToggleVisibilityHandler() }
+                            variant="soft"
+                            size="sm"
+                          />
+                        </ListItem>
+                      ))                    
+                    }
+                  </List>
+                </ListItem>
               )
             })
           }
