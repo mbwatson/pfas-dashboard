ee30a04750e1b3db37d96af38947836a88ff049f
Matt Watson <mwatson@renci.org>
2024-05-27 20:39:42 -0400

    tweak chart card  titles

--- src/views/dashboard/chart.js
+++ src/views/dashboard/chart.js
@@ -29,12 +29,12 @@ export const ChartView = () => {
 
       <Grid container spacing={ 2 } sx={{ pr: 2 }}>
         <Grid xs={ 12 } sm={ 12 } md={ 12 } lg={ 11 } xl={ 6 }>
-          <ChartCard title="Analytes Detection Counts">
+          <ChartCard title="Detection Frequencies">
             <ChemicalDetectionPieChart data={ table.getPrePaginationRowModel().rows } />
           </ChartCard>
         </Grid>
         <Grid xs={ 12 } sm={ 12 } md={ 12 } lg={ 11 } xl={ 6 }>
-          <ChartCard title="Analyte Counts in Various Media">
+          <ChartCard title="Frequencies by Medium">
             <ChemicalsByMediumRadarChart data={ table.getPrePaginationRowModel().rows } />
           </ChartCard>
         </Grid>
