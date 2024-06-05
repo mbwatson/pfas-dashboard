e82ee92a7642852f8242ac49bb74c98cbfb11ba7
Matt Watson <mwatson@renci.org>
2024-06-05 08:51:43 -0400

    improve fetch status messaging
    add api connection error notice

--- src/context/data.js
+++ src/context/data.js
@@ -10,8 +10,16 @@ import {
   useMemo,
   useState,
 } from 'react'
-import { CircularProgress } from '@mui/joy'
 import PropTypes from 'prop-types'
+import {
+  CircularProgress,
+  Sheet,
+  Stack,
+  Typography,
+} from '@mui/joy'
+import {
+  ErrorOutline as ErrorIcon
+} from '@mui/icons-material'
 import axios from 'axios'
 
 import { QueryClient, useQuery } from '@tanstack/react-query'
@@ -40,9 +48,48 @@ import { podmColumns } from '@components/table'
 const API_URL = `${ process.env.API_HOST }/podm/api`
 const CACHE_KEY = 'PFAS_DATA_CACHE'
 
+const CONNECTION_STATE_ICONS = {
+  busy: <CircularProgress size="sm" />,
+  error: <ErrorIcon color="error" />
+}
+
+const ConnectionStatus = ({
+  message = '',
+  status = 'busy',
+}) => {
+  return (
+    <Sheet
+      component={ Stack }
+      variant="solid"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        position: 'absolute',
+        left: 0, right: 0,
+        top: 0, bottom: 0,
+        '.MuiTypography-root': {
+          p: 2,
+        },
+      }}
+    >
+      <Typography
+        level="body-xl"
+        variant="soft"
+        startDecorator={ CONNECTION_STATE_ICONS[status] }
+      >{ message }</Typography>
+    </Sheet>
+  )
+}
+
+ConnectionStatus.propTypes = {
+  message: PropTypes.string,
+  status: PropTypes.oneOf(Object.keys(CONNECTION_STATE_ICONS)),
+}
+
 // this hook is responsible for fetching & refreshing api tokens
 const useToken = () => {
   const [accessToken, setAccessToken] = useState(null)
+  const [error, setError] = useState(null)
 
   const fetchToken = useCallback(async () => {
     try {
@@ -65,6 +112,7 @@ const useToken = () => {
       setAccessToken(access)
     } catch (error) {
       console.error(error.message)
+      setError('An error occurred.')
       return
     }
   }, [])
@@ -73,7 +121,7 @@ const useToken = () => {
     fetchToken()
   }, [])
 
-  return { accessToken, fetchToken }
+  return { accessToken, error, fetchToken }
 }
 
 // we want tanstack's queryClient available within our data context,
@@ -192,7 +240,11 @@ export const DataWrangler = ({ accessToken, children }) => {
       },
       filterCount,
     }}>
-      { pfasDataQuery.isPending ? <CircularProgress sx={{ margin: '15rem auto' }} /> : children }
+      {
+        pfasDataQuery.isPending || pfasDataQuery.isLoading
+          ? <ConnectionStatus message="Fetching data" />
+          : children
+        }
     </DataContext.Provider>
   )
 }
@@ -222,14 +274,23 @@ const queryClient = new QueryClient({
 export const DataProvider = ({ children }) => {
   const preferences = usePreferences()
 
-  const { accessToken, refreshing } = useToken()
+  const { accessToken, error } = useToken()
 
-  if (!accessToken && !refreshing) {
-    return <div>API: establishing connection...</div>
+  if (!accessToken && !error) {
+    return (
+      <ConnectionStatus
+        message="Establishing connection to API"
+      />
+    )
   }
 
-  if (!accessToken && refreshing) {
-    return <div>API: refreshing connection...</div>
+  if (!accessToken && error) {
+    return (
+      <ConnectionStatus
+        message={ error }
+        status="error"
+      />
+    )
   }
 
   return (
