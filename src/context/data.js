4009ba73b0cee428ba6288d86255b70592875ab2
Matt Watson <mwatson@renci.org>
2024-06-18 20:14:49 -0400

    extract api funcs to utils

--- src/context/data.js
+++ src/context/data.js
@@ -42,6 +42,7 @@ import {
   useReactTable,
 } from '@tanstack/react-table'
 import { podmColumns } from '@components/table'
+import { fetchSampleData, useToken } from '@util'
 
 //
 
@@ -86,112 +87,12 @@ ConnectionStatus.propTypes = {
   status: PropTypes.oneOf(Object.keys(CONNECTION_STATE_ICONS)),
 }
 
-// this hook is responsible for fetching & refreshing api tokens
-const useToken = () => {
-  const [accessToken, setAccessToken] = useState(null)
-  const [error, setError] = useState(null)
-
-  const fetchToken = useCallback(async () => {
-    try {
-      const response = await axios.post(
-        // the trailing slash is necessary here,
-        // as it matches urls.py in the backend.
-        `${ API_URL }/token/`,
-        {
-          username: process.env.API_USERNAME,
-          password: process.env.API_PASSWORD,
-        },
-      )
-      if (response.status !== 200) {
-        throw new Error('invalid response')
-      }
-      const { access, refresh } = await response.data
-      if (!access || !refresh) {
-        throw new Error('could not locate tokens')
-      }
-      setAccessToken(access)
-    } catch (error) {
-      console.error(error.message)
-      setError('An error occurred.')
-      return
-    }
-  }, [])
-
-  useEffect(() => {
-    fetchToken()
-  }, [])
-
-  return { accessToken, error, fetchToken }
-}
 
 // we want tanstack's queryClient available within our data context,
 // so we create a wrapper that provides tanstack query functionality.
 
 const DataContext = createContext({ })
 export const useData = () => useContext(DataContext)
-
-const createMultiQuerier = (endpoint, accessToken) => async () => {
-  console.info(`fetching data from ${ API_URL }/${ endpoint }...`)
-
-  if (!accessToken) {
-    console.log('no access token')
-    return []
-  }
-
-  const getFirstPage = async () => {
-    try {
-      const { data } = await axios.get(
-        `${ API_URL }/${ endpoint }?page=1&psize=1`,
-        {
-          timeout: 1000 * 5, // 5 seconds
-          headers: {
-            Authorization: `Bearer ${ accessToken }`,
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-      return data
-    } catch (error) {
-      console.error(error.message)
-      return null
-    }
-  }
-
-  // first, let's get the first page and check the total number of pages.
-  const data = await getFirstPage()
-
-  if (!data.count) {
-    return []
-  }
-
-  // if we're here, we have a non-zero number of pages,
-  // so we make the neccssary number of requests.
-  const PER_PAGE = 100
-  const promises = [...Array(Math.ceil(data.count / PER_PAGE)).keys()]
-    .map(p => axios(
-      `${ API_URL }/${ endpoint }?page=${ p + 1 }&psize=${ PER_PAGE }`,
-      {
-        timeout: 1000 * 5, // 5 seconds
-        headers: {
-          Authorization: `Bearer ${ accessToken }`,
-          'Content-Type': 'application/json',
-        },
-      }
-    ))
-
-  // return all results in one array.
-  return Promise.all(promises)
-    .then(responses => responses.map(r => r.data.results)
-      .reduce((allData, d) => {
-        allData.push(...d)
-        return allData
-      }, [])
-    )
-    .catch(error => {
-      console.error(error)
-      return []
-    })
-}
 
 export const DataWrangler = ({ accessToken, children }) => {
   const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 25 })
@@ -200,7 +101,7 @@ export const DataWrangler = ({ accessToken, children }) => {
 
   const pfasDataQuery = useQuery({
     queryKey: ['pfas_sample_data'],
-    queryFn: createMultiQuerier('pfas_sample_data', accessToken),
+    queryFn: fetchSampleData(accessToken),
   })
 
   const analyteIds = useMemo(() => analytes.map(s => s.id).sort(), [])
