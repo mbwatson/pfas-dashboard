d6c1502abb883e8e7ce92b816a5a9a402257e41e
Matt Watson <mwatson@renci.org>
2024-06-18 20:26:28 -0400

    reduce to used analytes

--- src/data/analytes.js
+++ src/data/analytes.js
@@ -1,122 +1,10 @@
 export const analytes = [
   {
-    id: 'pfunda',
-    name: 'Perfluoroundecanoic acid',
-    abbreviation: 'PFUnDA',
-    dtxsid: 'DTXSID8047553',
-    formula: 'C11HF21O2'
-  },
-  {
-    id: 'pftrda',
-    name: 'Perfluorotridecanoic acid',
-    abbreviation: 'PFTrDA',
-    dtxsid: 'DTXSID90868151',
-    formula: 'C13HF25O2'
-  },
-  {
-    id: 'pfteda',
-    name: 'Perfluorotetradacanoic acid',
-    abbreviation: 'PFTeDA',
-    dtxsid: 'DTXSID3059921',
-    formula: 'C14HF27O2'
-  },
-  {
-    id: 'pfuda',
-    name: 'Perfluoroundecanoic acid',
-    abbreviation: 'PFUDA',
-    dtxsid: 'DTXSID8047553',
-    formula: 'C11HF21O2'
-  },
-  {
-    id: 'pfpra',
-    name: 'Perfluoropropanoic acid',
-    abbreviation: 'PFPrA',
-    dtxsid: 'DTXSID8059970',
-    formula: 'C3HF5O2'
-  },
-  {
     id: 'pfba',
     name: 'Perfluorobutanoic acid',
     abbreviation: 'PFBA',
     dtxsid: 'DTXSID4059916',
     formula: 'C4HF7O2'
-  },
-  {
-    id: 'pfpea',
-    name: 'Perfluoropentanoic acid',
-    abbreviation: 'PFPeA',
-    dtxsid: 'DTXSID6062599',
-    formula: 'C5HF9O2'
-  },
-  {
-    id: 'pfhxa',
-    name: 'Perfluorohexanoic acid',
-    abbreviation: 'PFHxA',
-    dtxsid: 'DTXSID3031862',
-    formula: 'C6HF11O2'
-  },
-  {
-    id: 'pfhpa',
-    name: 'Perfluoroheptanoic acid',
-    abbreviation: 'PFHpA',
-    dtxsid: 'DTXSID1037303',
-    formula: 'C7HF13O2'
-  },
-  {
-    id: 'pfoa',
-    name: 'Perfluorooctanoic acid',
-    abbreviation: 'PFOA',
-    dtxsid: 'DTXSID8031865',
-    formula: 'C8HF15O2'
-  },
-  {
-    id: 'pfna',
-    name: 'Perfluorononanoic acid',
-    abbreviation: 'PFNA',
-    dtxsid: 'DTXSID8031863',
-    formula: 'C9HF17O2'
-  },
-  {
-    id: 'pfda',
-    name: 'Perfluorodecanoic acid',
-    abbreviation: 'PFDA',
-    dtxsid: 'DTXSID3031860',
-    formula: 'C10HF19O2'
-  },
-  {
-    id: 'pfdoa',
-    name: 'Perfluorododecanoic acid',
-    abbreviation: 'PFDoA',
-    dtxsid: 'DTXSID8031861',
-    formula: 'C12HF23O2'
-  },
-  {
-    id: 'pfta',
-    name: 'Perfluorotetradecanoic acid',
-    abbreviation: 'PFTA',
-    dtxsid: 'DTXSID3059921',
-    formula: 'C14HF27O2'
-  },
-  {
-    id: 'pfhxda',
-    name: 'Perfluorohexadecanoic acid',
-    abbreviation: 'PFHxDA',
-    dtxsid: 'DTXSID1070800',
-    formula: 'C16HF31O2'
-  },
-  {
-    id: 'pfoda',
-    name: 'Perfluorooctadecanoic acid',
-    abbreviation: 'PFODA',
-    dtxsid: 'DTXSID1066071',
-    formula: 'C18HF35O2'
-  },
-  {
-    id: 'pfprs',
-    name: 'Perfluoropropanesulfonic acid',
-    abbreviation: 'PFPrS',
-    dtxsid: 'DTXSID30870531',
-    formula: 'C3HF7O3S'
   },
   {
     id: 'pfbs',
@@ -126,39 +14,11 @@ export const analytes = [
     formula: 'C4HF9O3S'
   },
   {
-    id: 'pfpes',
-    name: 'Perfluoropentanesulfonic acid',
-    abbreviation: 'PFPeS',
-    dtxsid: 'DTXSID8062600',
-    formula: 'C5HF11O3S'
-  },
-  {
-    id: 'pfhxs',
-    name: 'Perfluorohexanesulfonic acid',
-    abbreviation: 'PFHxS',
-    dtxsid: 'DTXSID7040150',
-    formula: 'C6HF13O3S'
-  },
-  {
-    id: 'pfhps',
-    name: 'Perfluoroheptanesulfonic acid',
-    abbreviation: 'PFHpS',
-    dtxsid: 'DTXSID8059920',
-    formula: 'C7HF15O3S'
-  },
-  {
-    id: 'pfos',
-    name: 'Perfluorooctanesulfonic acid',
-    abbreviation: 'PFOS',
-    dtxsid: 'DTXSID3031864',
-    formula: 'C8HF17O3S'
-  },
-  {
-    id: 'pfns',
-    name: 'Perfluorononanesulfonic acid',
-    abbreviation: 'PFNS',
-    dtxsid: 'DTXSID8071356',
-    formula: 'C9HF19O3S'
+    id: 'pfda',
+    name: 'Perfluorodecanoic acid',
+    abbreviation: 'PFDA',
+    dtxsid: 'DTXSID3031860',
+    formula: 'C10HF19O2'
   },
   {
     id: 'pfds',
@@ -168,80 +28,80 @@ export const analytes = [
     formula: 'C10HF21O3S'
   },
   {
-    id: 'pfdos',
-    name: 'Perfluorododecanesulfonic acid',
-    abbreviation: 'PFDoS',
-    dtxsid: 'DTXSID20873011',
-    formula: 'C12HF25O3S'
+    id: 'pfhpa',
+    name: 'Perfluoroheptanoic acid',
+    abbreviation: 'PFHpA',
+    dtxsid: 'DTXSID1037303',
+    formula: 'C7HF13O2'
   },
   {
-    id: 'pfosa',
-    name: 'Perfluorooctanesulfonamide',
-    abbreviation: 'PFOSA',
-    dtxsid: 'DTXSID3038939',
-    formula: 'C8H2F17NO2S'
+    id: 'pfhps',
+    name: 'Perfluoroheptanesulfonic acid',
+    abbreviation: 'PFHpS',
+    dtxsid: 'DTXSID8059920',
+    formula: 'C7HF15O3S'
   },
   {
-    id: 'pfmoaa',
-    name: 'Perfluoro-2-methoxyacetic acid',
-    abbreviation: 'PFMOAA',
-    dtxsid: 'DTXSID00408562',
-    formula: 'C3HF5O3'
+    id: 'pfhxa',
+    name: 'Perfluorohexanoic acid',
+    abbreviation: 'PFHxA',
+    dtxsid: 'DTXSID3031862',
+    formula: 'C6HF11O2'
   },
   {
-    id: 'pfmpa',
-    name: 'Perfluoro-3-methoxypropanoic acid',
-    abbreviation: 'PFMPA',
-    dtxsid: 'DTXSID70191136',
-    formula: 'C4HF7O3'
+    id: 'pfhxs',
+    name: 'Perfluorohexanesulfonic acid',
+    abbreviation: 'PFHxS',
+    dtxsid: 'DTXSID7040150',
+    formula: 'C6HF13O3S'
   },
   {
-    id: 'pfmba',
-    name: 'Perfluoro(4-methoxybutanoic) acid',
-    abbreviation: 'PFMBA',
-    dtxsid: 'DTXSID60500450',
-    formula: 'C5HF9O3'
+    id: 'pfna',
+    name: 'Perfluorononanoic acid',
+    abbreviation: 'PFNA',
+    dtxsid: 'DTXSID8031863',
+    formula: 'C9HF17O2'
   },
   {
-    id: 'PFPE-1',
-    name: 'Perfluoro-6-methyl-5-oxaheptanoic acid',
-    abbreviation: 'PFPE-1',
-    dtxsid: 'DTXSID60663110',
-    formula: 'C7HF13O3'
+    id: 'pfns',
+    name: 'Perfluorononanesulfonic acid',
+    abbreviation: 'PFNS',
+    dtxsid: 'DTXSID8071356',
+    formula: 'C9HF19O3S'
   },
   {
-    id: 'pfo2hxa',
-    name: 'Perfluoro-3,5-dioxahexanoic acid',
-    abbreviation: 'PFO2HxA',
-    dtxsid: 'DTXSID50892351',
-    formula: 'C4HF7O4 '
+    id: 'pfoa',
+    name: 'Perfluorooctanoic acid',
+    abbreviation: 'PFOA',
+    dtxsid: 'DTXSID8031865',
+    formula: 'C8HF15O2'
   },
   {
-    id: 'pfo3oa',
-    name: 'Perfluoro-3,5,7-trioxaoctanoic acid',
-    abbreviation: 'PFO3OA',
-    dtxsid: 'DTXSID20892348',
-    formula: 'C5HF9O5'
+    id: 'pfos',
+    name: 'Perfluorooctanesulfonic acid',
+    abbreviation: 'PFOS',
+    dtxsid: 'DTXSID3031864',
+    formula: 'C8HF17O3S'
   },
   {
-    id: 'pfo4da',
-    name: 'Perfluoro-3,5,7,9-butaoxadecanoic acid',
-    abbreviation: 'PFO4DA',
-    dtxsid: 'DTXSID90723993',
-    formula: 'C6HF11O6'
+    id: 'pfpea',
+    name: 'Perfluoropentanoic acid',
+    abbreviation: 'PFPeA',
+    dtxsid: 'DTXSID6062599',
+    formula: 'C5HF9O2'
   },
   {
-    id: 'pfo5da',
-    name: 'Perfluoro-3,5,7,9,11-pentaoxadodecanoic acid',
-    abbreviation: 'PFO5DA',
-    dtxsid: 'DTXSID50723994',
-    formula: 'C7HF13O7'
+    id: 'pfpes',
+    name: 'Perfluoropentanesulfonic acid',
+    abbreviation: 'PFPeS',
+    dtxsid: 'DTXSID8062600',
+    formula: 'C5HF11O3S'
   },
   {
-    id: 'pfeesa',
-    name: 'Perfluoro(2-ethoxyethane)sulfonic acid',
-    abbreviation: 'PFEESA',
-    dtxsid: 'DTXSID50379814',
-    formula: 'C4HF9O4S'
-  }
+    id: 'pfunda',
+    name: 'Perfluoroundecanoic acid',
+    abbreviation: 'PFUnDA',
+    dtxsid: 'DTXSID8047553',
+    formula: 'C11HF21O2'
+  },
 ]
